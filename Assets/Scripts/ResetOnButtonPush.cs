5a1c5dc5d083b4e8e4bbe5c2b30d0d2dc8ee244b
Unknown <Sebdami@hotmail.fr>
2017-12-19 16:37:21 +0100

    Add some things

---
+++ Assets/Scripts/ResetOnButtonPush.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetOnButtonPush : MonoBehaviour {
+    [SerializeField]
+    Transform startPos;
+    Rigidbody rb;
+    public float startForce = 3.0f;
+    public float forceToAdd = 0.0f;
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+        Reset();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(SteamVR_Controller.Input(1).GetHairTriggerDown() || SteamVR_Controller.Input(0).GetHairTriggerDown() || Input.GetKeyDown(KeyCode.Space))
+        {
+            Reset();
+        }
+	}
+
+    private void Reset()
+    {
+        transform.position = startPos.position;
+        rb.velocity = Vector3.zero;
+        rb.AddForce(new Vector3(Random.Range(-0.15f, 0.15f), 0, Random.Range(-1.3f, -0.7f)) *(startForce + forceToAdd + Random.Range(-0.1f, 0.2f)), ForceMode.Impulse);
+    }
+}
---
+++ Assets/Scripts/ThrowableInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowableInitializer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<SpringJoint>().connectedBody = transform.parent.GetComponent<Rigidbody>();
+        transform.GetChild(1).GetComponent<ConnectLines>().tr1 = transform.parent;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
